2bee6cd4a7ee3b0d871131eb70282cca000b11e3
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-14 15:43:01 -0400

    Creates snapshot and render test

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,19 @@
 // Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Display from './Display';
+
+
+// Snapshot 
+describe('<Display />', () => {
+    it('should match snapshot', () => {
+        const tree = render(<Display />);
+        expect(tree).toMatchSnapshot();
+    })
+})
+
+// Render test
+test('display renders correctly', () => {
+    render(<Display />)
+})
